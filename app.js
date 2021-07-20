12ec865bae837276576c969d9dc20f3e638ac2c9
Arpita1899 <arpitaverma8885@gmail.com>
2021-07-20 22:03:35 +0530

    added functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var errorDisplay=document.querySelector("#errorDisplay");
 var checkAmt=document.querySelector("#check-amt");
 var checkBtn=document.querySelector("#check-btn");
 var showAmt=document.querySelector(".return");
+var noOfNotes=document.querySelectorAll(".noOfNotes");
 
 
 var cashPaid=document.querySelector('.cash-paid');
@@ -22,23 +23,62 @@ inputBtn.addEventListener('click',()=>{
 })
 
 checkBtn.addEventListener('click',()=>{
+    clearNotes();
     hideError();
-    if(Number(checkAmt.value)>0){
-        showAmt.style.display="block";
-        checkBtn.style.display="none";
+    var billAmtInput=Number(inputBillAmt.value);
+    var cashPaidAmt=Number(checkAmt.value);
+    if(cashPaidAmt>0){
+        if(!Number.isInteger(cashPaidAmt)){
+            showError("Please enter a vaild Amount!")
+            return;
+        }
+    }
+    if(cashPaidAmt<billAmtInput){
+        showError("please pay a valid Amount!");
+        return;
     }
     else{
-        showError("Please enter a valid amount!")
+   
+    calculateReturn(billAmtInput,cashPaidAmt);
     }
+
     
 })
 
    
 const notes=[2000,500,100,20,10,5,1];
 
-function calculateReturn(inputBillAmt,checkAmt){
+function calculateReturn(billAmt,paidAmt){
+    var returnAmt=paidAmt-billAmt;
+    if(returnAmt<1){
+        showError("No cash to be returned");
+        return;
+    }
+    showAmt.style.display="block";
+
+    for(var i=0;i<notes.length;i++){
+        returnAmt=compare(returnAmt,notes[i],i);
+    }
+
     
 }
+function compare(remainder,note,index){
+    if(remainder>=note){
+        var notesReturned=Math.floor(remainder/note);
+        remainder=remainder-notesReturned*note;
+        noOfNotes[index].innerText=`${notesReturned}`;
+    }
+    return remainder;
+}
+
+function clearNotes(){
+    for(var notesReturned of noOfNotes){
+        notesReturned.innerText="";
+        showAmt.style.display="none";
+    }
+}
+
+
 function showError(text){
     errorDisplay.style.display="block";
     errorDisplay.innerText=text;
